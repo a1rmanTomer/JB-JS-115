f55776536d9f19d5f3af3700ccd4abbb28321656
a1rmanTomer <extramine7@gmail.com>
2025-01-29 18:23:51 +0200

    a

---
+++ jan29/test.js
@@ -0,0 +1,8 @@
+console.log("Js is running.")
+
+const tomerInfo = ["Tomer", "Dan", "(almost) 21", "GVT, Gush-Dan, IL", "In Active Duty Service"];
+
+console.log(tomerInfo)
+
+
+console.log("Js finished running.")
