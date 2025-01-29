8f8f34e16afd24fc0e8e8257906c252774da6ad7
a1rmanTomer <extramine7@gmail.com>
2025-01-29 19:21:42 +0200

    a

--- jan29/test.js
+++ jan29/test.js
@@ -1,18 +1,32 @@
 console.log("Js is running.")
 
+// ex-1
 function printTomer(){
     const tomerInfo = ["Tomer", "Dan", "(almost) 21", "GVT, Gush-Dan, IL", "In Active Duty Service"]
     console.log(tomerInfo)
 }
-
 printTomer()
 
 
 let fName = "Tomer"
 let lName = "Dan"
 let fullName = `${new Date().toISOString()} ${">"} ${fName} ${lName} ${"has used TOKEN:"} ${Math.ceil(Math.random()*9674856798789)}`
-
 console.log(fullName)
 
+
+// ex-2
+function printMetaTomer(){
+    let email = "tomerdan1404@gmail.com"
+    let job = "Active Duty Soldier"
+    let title = "Mr."
+    let age = 21
+    let isEmployed = true
+    let hasCar = false
+
+    let tomerMetaDate = `${title} ${fName} ${lName}, Email: ${email}, Job: ${job}, Age: ${age}, Employed: ${isEmployed}, Has Car: ${hasCar}`
+
+    console.log(tomerMetaDate)
+}
+printMetaTomer()
 
 console.log("Js finished running.")
