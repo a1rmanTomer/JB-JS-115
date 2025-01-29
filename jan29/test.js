4f25e9448a8aa27ccff80241af677640e087a64b
a1rmanTomer <extramine7@gmail.com>
2025-01-29 18:41:26 +0200

    a

--- jan29/test.js
+++ jan29/test.js
@@ -1,10 +1,10 @@
 console.log("Js is running.")
 
 function printTomer(){
-    const tomerInfo = ["Tomer", "Dan", "(almost) 21", "GVT, Gush-Dan, IL", "In Active Duty Service"];
+    const tomerInfo = ["Tomer", "Dan", "(almost) 21", "GVT, Gush-Dan, IL", "In Active Duty Service"]
     console.log(tomerInfo)
 }
 
-printTomer();
+printTomer()
 
 console.log("Js finished running.")
