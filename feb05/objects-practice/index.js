ceaeca7601ee11691e8f610689445acf59b10dab
a1rmanTomer <extramine7@gmail.com>
2025-02-05 20:31:53 +0200

    f

--- feb05/objects-practice/index.js
+++ feb05/objects-practice/index.js
@@ -1,12 +1,58 @@
 console.log("Script start");
 
+// EX1
+let myCar = {
+    make: "Hyundai",
+    model: "I20n",
+    year: 2024,
+    type: "Hot Hatch",
+    engine: "1.6L-TGDI",
+    color: "N-Blue",
+    doors: 5,
+    km: 49250
+}
+
+// print func:
+function printCar(car){
+    console.log(`Color: ${car}`);
+}
+
+// print car color:
+printCar(myCar.color)
+
+// reset color:
+myCar.color = "N-Red";
 
 
+// reprint car color:
+printCar(myCar.color)
 
+// func to add KM
+function addKm(kmToAdd){
+    myCar.km += kmToAdd
+}
 
+// EX2
+function pDetails(id, price, name){
+    // input val
+    if (typeof id !== 'string')
+        id = id.toString();
+    if (typeof name !== 'string')
+        id = id.toString()
+    if (typeof price !== 'number')
+        price = Number(price)
 
+    let currentTime = new Date().toString();
 
+    let myObj = {
+        pId: id,
+        pName: name,
+        pCreateTimeStamp: currentTime,
+        pPrice: price
+    }
 
+    console.log(myObj);
+}
 
 
 
