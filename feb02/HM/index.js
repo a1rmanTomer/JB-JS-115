b364180eeffb82eef018f59d7eba417c6021394e
a1rmanTomer <extramine7@gmail.com>
2025-02-05 17:36:53 +0200

    final-HM

--- feb02/HM/index.js
+++ feb02/HM/index.js
@@ -38,7 +38,7 @@ function randomNumbers(min, max){
 }
 
 // dec for array tests
-for (let i = 0; i < 31; i++) {
+for (let i = 0; i < 30; i++) {
     julyTemp2000.push(randomNumbers(35,45));
     julyTemp2001.push(randomNumbers(31,44));
 }
